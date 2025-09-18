4200a44d04a46776066fb671302c0ca94ca11fd8
pie-suhee <120539985+pie-suhee@users.noreply.github.com>
2025-09-18 13:20:05 +0900

    준비중 페이지 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Footer = ({ type }) => {
     const { userType, footerType } = type;
 
@@ -7,46 +9,58 @@ const Footer = ({ type }) => {
         <footer className={`${userType} ${footerType}`}>
             <ul>
                 <li className='home'>
-                    <figure className={isActive('home')}>
-                        <img src="/img/footer/home_off.svg" alt="홈 버튼" />
-                        <img src="/img/footer/home_on.svg" alt="홈 버튼" />
-                        <figcaption className="caption_11">홈</figcaption>
-                    </figure>
+                    <Link to="/member/home">
+                        <figure className={isActive('home')}>
+                            <img src="/img/footer/home_off.svg" alt="홈 버튼" />
+                            <img src="/img/footer/home_on.svg" alt="홈 버튼" />
+                            <figcaption className="caption_11">홈</figcaption>
+                        </figure>
+                    </Link>
                 </li>
                 <li className='schedule'>
-                    <figure className={isActive('schedule')}>
-                        <img src="/img/footer/schedule_off.svg" alt="스케줄 버튼" />
-                        <img src="/img/footer/schedule_on.svg" alt="스케줄 버튼" />
-                        <figcaption className="caption_11">스케줄</figcaption>
-                    </figure>
+                    <Link to="/error">
+                        <figure className={isActive('schedule')}>
+                            <img src="/img/footer/schedule_off.svg" alt="스케줄 버튼" />
+                            <img src="/img/footer/schedule_on.svg" alt="스케줄 버튼" />
+                            <figcaption className="caption_11">스케줄</figcaption>
+                        </figure>
+                    </Link>
                 </li>
                 <li className='note'>
-                    <figure className={isActive('note')}>
-                        <img src="/img/footer/note_off.svg" alt="채팅 버튼" />
-                        <img src="/img/footer/note_on.svg" alt="채팅 버튼" />
-                        <figcaption className="caption_11">헬스 노트</figcaption>
-                    </figure>
+                    <Link to="/error">
+                        <figure className={isActive('note')}>
+                            <img src="/img/footer/note_off.svg" alt="채팅 버튼" />
+                            <img src="/img/footer/note_on.svg" alt="채팅 버튼" />
+                            <figcaption className="caption_11">헬스 노트</figcaption>
+                        </figure>
+                    </Link>
                 </li>
                 <li className='chat'>
-                    <figure className={isActive('chat')}>
-                        <img src="/img/footer/chat_off.svg" alt="채팅 버튼" />
-                        <img src="/img/footer/chat_on.svg" alt="채팅 버튼" />
-                        <figcaption className="caption_11">채팅</figcaption>
-                    </figure>
+                    <Link to="/error">
+                        <figure className={isActive('chat')}>
+                            <img src="/img/footer/chat_off.svg" alt="채팅 버튼" />
+                            <img src="/img/footer/chat_on.svg" alt="채팅 버튼" />
+                            <figcaption className="caption_11">채팅</figcaption>
+                        </figure>
+                    </Link>
                 </li>
                 <li className='management'>
-                    <figure className={isActive('management')}>
-                        <img src="/img/footer/management_off.svg" alt="회원 관리 버튼" />
-                        <img src="/img/footer/management_on.svg" alt="회원 관리 버튼" />
-                        <figcaption className="caption_11">회원 관리</figcaption>
-                    </figure>
+                    <Link to="/error">
+                        <figure className={isActive('management')}>
+                            <img src="/img/footer/management_off.svg" alt="회원 관리 버튼" />
+                            <img src="/img/footer/management_on.svg" alt="회원 관리 버튼" />
+                            <figcaption className="caption_11">회원 관리</figcaption>
+                        </figure>
+                    </Link>
                 </li>
                 <li className='my'>
-                    <figure className={isActive('my')}>
-                        <img src="/img/footer/my_off.svg" alt="마이 버튼" />
-                        <img src="/img/footer/my_on.svg" alt="마이 버튼" />
-                        <figcaption className="caption_11">마이</figcaption>
-                    </figure>
+                    <Link to="/error">
+                        <figure className={isActive('my')}>
+                            <img src="/img/footer/my_off.svg" alt="마이 버튼" />
+                            <img src="/img/footer/my_on.svg" alt="마이 버튼" />
+                            <figcaption className="caption_11">마이</figcaption>
+                        </figure>
+                    </Link>
                 </li>
             </ul>
         </footer>
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,17 @@
 import Header from "../components/Header"
 
+import '../css/error.css';
+
 function Error() {
   return (
     <>
       <Header type="1000000000000000" />
+      <div className="error">
+        <img src={require(`../assets/img/admin_char.svg`).default} className="admin_char" alt="관리자 캐릭터" />
+        <span className="large_title_24">
+          페이지 준비 중입니다.
+        </span>
+      </div>
     </>
   );
 }
